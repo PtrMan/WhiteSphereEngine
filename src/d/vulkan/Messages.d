4715951e23ff2bced9712f584a93084a5ffa6065
PtrMan <robertw89@googlemail.com>
2016-02-26 11:59:36 +0100

    messages for frequently used vulkan error messages

---
+++ src/d/vulkan/Messages.d
@@ -0,0 +1,6 @@
+module vulkan.Messages;
+
+/**
+ * one single central place for common vulkan related messages
+ */
+public static const string COULDNT_COMMAND_BUFFER = "Couldn't allocate command buffer!";
