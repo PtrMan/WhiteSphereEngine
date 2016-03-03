d9630569d9783322005afed11b0c2a894f18d21f
PtrMan <robertw89@googlemail.com>
2016-03-03 23:34:47 +0100

    overhauled startscript, still utdated, more debugging

--- src/d/toIntegrate/debugVulkan.bat
+++ src/d/toIntegrate/debugVulkan.bat
@@ -3,6 +3,11 @@ rem VK_LAYER_LUNARG_api_dump
 set  VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_api_dump;VK_LAYER_LUNARG_param_checker
 set  VK_DEVICE_LAYERS=VK_LAYER_LUNARG_api_dump;VK_LAYER_LUNARG_param_checker
 
+rem extreme
+
+set  VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_api_dump;VK_LAYER_LUNARG_swapchain;VK_LAYER_LUNARG_standard_validation
+set  VK_DEVICE_LAYERS=VK_LAYER_LUNARG_api_dump;VK_LAYER_LUNARG_swapchain;VK_LAYER_LUNARG_standard_validation
+
 rem set  VK_INSTANCE_LAYERS=
 rem set  VK_DEVICE_LAYERS=
 
