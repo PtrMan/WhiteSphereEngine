86025259c1dfc6802b3542ab07b1c29e631fa7db
PtrMan <robertw89@googlemail.com>
2016-03-23 22:42:46 +0100

    Matrix: added vector multiplication

--- src/d/math/MatrixCommonOperations.d
+++ src/d/math/MatrixCommonOperations.d
@@ -6,7 +6,7 @@ import AlgebraLib.InstructionTranslator;
 import AlgebraLib.CompiletimeMatrixDescriptor;
 import AlgebraLib.ResultDescriptor;
 
-private string generateDOfMatrixMultiply(uint[] dimensions, string matrixnameA, string matrixnameB, string resultMatrixName) {
+private string generateDOfMatrixMultiplyForMatrix(uint[] dimensions, string matrixnameA, string matrixnameB, string resultMatrixName) {
 	InstructionTranslator instructionTranslator;
 
 	CompiletimeMatrixDescriptor matrixA;
@@ -15,24 +15,24 @@ private string generateDOfMatrixMultiply(uint[] dimensions, string matrixnameA, 
 
 	matrixA = new CompiletimeMatrixDescriptor();
 
-	matrixA.alignment = 4;
+	//matrixA.alignment = 4;
 	matrixA.dimensions = dimensions;
 	matrixA.pointerName = matrixnameA;
-	matrixA.datatype = CompiletimeMatrixDescriptor.EnumDatatype.DOUBLE;
+	//matrixA.datatype = CompiletimeMatrixDescriptor.EnumDatatype.DOUBLE;
 
 	matrixB = new CompiletimeMatrixDescriptor();
 
-	matrixB.alignment = 4;
+	//matrixB.alignment = 4;
 	matrixB.dimensions = dimensions;
 	matrixB.pointerName = matrixnameB;
-	matrixB.datatype = CompiletimeMatrixDescriptor.EnumDatatype.DOUBLE;
+	//matrixB.datatype = CompiletimeMatrixDescriptor.EnumDatatype.DOUBLE;
 
 	resultMatrix = new CompiletimeMatrixDescriptor();
 
-	resultMatrix.alignment = 4;
+	//resultMatrix.alignment = 4;
 	resultMatrix.dimensions = dimensions;
 	resultMatrix.pointerName = resultMatrixName;
-	resultMatrix.datatype = CompiletimeMatrixDescriptor.EnumDatatype.DOUBLE;
+	//resultMatrix.datatype = CompiletimeMatrixDescriptor.EnumDatatype.DOUBLE;
 
 	ResultDescriptor result;
 
@@ -43,14 +43,59 @@ private string generateDOfMatrixMultiply(uint[] dimensions, string matrixnameA, 
 	return instructionTranslator.emitDSource();
 }
 
-private template generateDOfMatrixMultiplyTemplate(uint[] dimensions, string matrixnameA, string matrixnameB, string resultMatrixName) {
-	const char[] generateDOfMatrixMultiplyTemplate = generateDOfMatrixMultiply(dimensions, matrixnameA, matrixnameB, resultMatrixName);
+private string generateDOfMatrixMultiplyForVector(uint[] dimensions, string matrixnameA, string matrixnameB, string resultMatrixName) {
+	InstructionTranslator instructionTranslator;
+
+	CompiletimeMatrixDescriptor matrixA;
+	CompiletimeMatrixDescriptor matrixB;
+	CompiletimeMatrixDescriptor resultMatrix;
+
+	matrixA = new CompiletimeMatrixDescriptor();
+
+	//matrixA.alignment = 4;
+	matrixA.dimensions = dimensions;
+	matrixA.pointerName = matrixnameA;
+	//matrixA.datatype = CompiletimeMatrixDescriptor.EnumDatatype.DOUBLE;
+
+	matrixB = new CompiletimeMatrixDescriptor();
+
+	//matrixB.alignment = 4;
+	matrixB.dimensions = [1, dimensions[1]];
+	matrixB.pointerName = matrixnameB;
+	//matrixB.datatype = CompiletimeMatrixDescriptor.EnumDatatype.DOUBLE;
+
+	resultMatrix = new CompiletimeMatrixDescriptor();
+
+	//resultMatrix.alignment = 4;
+	resultMatrix.dimensions = [1, dimensions[1]];
+	resultMatrix.pointerName = resultMatrixName;
+	//resultMatrix.datatype = CompiletimeMatrixDescriptor.EnumDatatype.DOUBLE;
+
+	ResultDescriptor result;
+
+	instructionTranslator = new InstructionTranslator(InstructionTranslator.EnumPerformanceTarget.EXTREMESPEED, InstructionTranslator.EnumInstructionSetTarget.DUMMY);
+
+	instructionTranslator.multiplyMatrix(matrixA, matrixB, resultMatrix, result);
+
+	return instructionTranslator.emitDSource();
+}
+
+private template generateDOfMatrixMultiplyForMatrixTemplate(uint[] dimensions, string matrixnameA, string matrixnameB, string resultMatrixName) {
+	const char[] generateDOfMatrixMultiplyForMatrixTemplate = generateDOfMatrixMultiplyForMatrix(dimensions, matrixnameA, matrixnameB, resultMatrixName);
+}
+
+private template generateDOfMatrixMultiplyForVectorTemplate(uint[] dimensions, string matrixnameA, string matrixnameB, string resultMatrixName) {
+	const char[] generateDOfMatrixMultiplyForVectorTemplate = generateDOfMatrixMultiplyForVector(dimensions, matrixnameA, matrixnameB, resultMatrixName);
 }
 
 public void mul(uint width, uint height)(Matrix!(double, width, height) a, Matrix!(double, width, height) b, Matrix!(double, width, height) result) {
-	mixin(generateDOfMatrixMultiplyTemplate!([width, height], "a", "b", "result"));
+	mixin(generateDOfMatrixMultiplyForMatrixTemplate!([width, height], "a", "b", "result"));
 }
 
+public void mulVector(uint width, uint height)(Matrix!(double, width, height) a, Matrix!(double, 1, height) b, Matrix!(double, 1, height) result) {
+	mixin(generateDOfMatrixMultiplyForVectorTemplate!([width, height], "a", "b", "result"));
+}
+	
 unittest {
 	Matrix!(double, 2, 2) a = new Matrix!(double, 2, 2)();
 	Matrix!(double, 2, 2) b = new Matrix!(double, 2, 2)();
@@ -75,4 +120,23 @@ unittest {
 	assert(abs(result[0,1]) < 0.00001);
 	assert(abs(result[1,0]) < 0.00001);
 	assert(result[1,1] == 1.0);
+}
+
+unittest {
+	Matrix!(double, 2, 2) a = new Matrix!(double, 2, 2)();
+	Matrix!(double, 1, 2) b = new Matrix!(double, 1, 2)();
+	Matrix!(double, 1, 2) result = new Matrix!(double, 1, 2)();
+	
+	a[0, 0] = 1.0;
+	a[0, 1] = 0.0;
+	a[1, 0] = 0.0;
+	a[1, 1] = 1.0;
+	
+	b[0, 0] = 5.0;
+	b[1, 0] = 4.0;
+	
+	mulVector(a, b, result);
+	
+	assert(result[0, 0] == 5.0);
+	assert(result[1, 0] == 4.0);
 }
