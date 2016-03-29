0f29a3827346c2ba2d33796a6d764a70ce66486b
PtrMan <robertw89@googlemail.com>
2016-03-29 08:37:33 +0200

    added math.QuadraticSolver from my ProjectSci

---
+++ src/d/math/QuadraticSolver.d
@@ -0,0 +1,32 @@
+module math.QuadraticSolver;
+
+import std.math : sqrt;
+
+/** \brief solves the quadratic equation
+ *
+ * Formula : (TOFORMAT x*x*a + x*b + c = 0)
+ *
+ * \param a ...
+ * \param b ...
+ * \param c ...
+ * \param existSolutions ...
+ * \param solution1 ...
+ * \param solution2 ...
+ */
+public void quadraticSolve(Type)(Type a, Type b, Type c, out bool existSolutions, out Type solution1, out Type solution2) {
+	if( a == cast(Type)0.0 ) {
+ 		existSolutions = false;
+ 		return;
+	}
+		
+	Type toSquareRoot = B*B - cast(Type)4.0*a*c;
+	existSolutions = toSquareRoot > 0;
+	if( existSolutions ) {
+		Type squareRooted = sqrt(toSquareRoot);
+
+		solution1 = (-b + squareRooted) / (cast(Type)2.0*a);
+		solution2 = (-b - squareRooted) / (cast(Type)2.0*a);
+
+		existSolutions = true;
+	}
+}
