43188274bb60f38b58b1a08f066314d41ba03c9d
PtrMan <robertw89@googlemail.com>
2016-03-31 15:30:34 +0200

    refactored out AxisOrientedBoundingBox from HashSpatialNestedGrid, added merge methods to AABB

--- src/d/geometry/AxisOrientedBoundingBox.d
+++ src/d/geometry/AxisOrientedBoundingBox.d
@@ -12,6 +12,8 @@ class AxisOrientedBoundingBox(VectorType) {
 // bounding box helpers
 // TODO< put into own sourcefile >
 
+import std.math : min, max;
+
 import math.Range;
 
 bool boundingBoxIsInsidePositionInclusive(VectorType)(AxisOrientedBoundingBox!VectorType boundingBox, VectorType toCheckPosition) {
@@ -43,4 +45,27 @@ bool boundingBoxDoesOverlapInclusive(VectorType)(AxisOrientedBoundingBox!VectorT
 		overlapsOrInsideForOneDimension(boundingBox.min.x, boundingBox.max.x, toCheckBoundingBox.min.x, toCheckBoundingBox.max.x) &&
 		overlapsOrInsideForOneDimension(boundingBox.min.y, boundingBox.max.y, toCheckBoundingBox.min.y, toCheckBoundingBox.max.y) &&
 		overlapsOrInsideForOneDimension(boundingBox.min.z, boundingBox.max.z, toCheckBoundingBox.min.z, toCheckBoundingBox.max.z);
+}
+
+// creates a bounding box which includes both bounding boxes
+AxisOrientedBoundingBox!VectorType merge(VectorType)(AxisOrientedBoundingBox!VectorType a, AxisOrientedBoundingBox!VectorType b) {
+	alias typeof(boundingBox.min.x) NumericType;
+	
+	NumericType minX = min(a.min.x, b.min.x);
+	NumericType minY = min(a.min.y, b.min.y);
+	NumericType minZ = min(a.min.z, b.min.z);
+	NumericType maxX = max(a.max.x, b.max.x);
+	NumericType maxY = max(a.max.y, b.max.y);
+	NumericType maxZ = max(a.max.z, b.max.z);
+	return new AxisOrientedBoundingBox!VectorType(new VectorType(minX, minY, minZ), new VectorType(maxX, maxY, maxZ));
+}
+
+AxisOrientedBoundingBox!VectorType merge(VectorType)(AxisOrientedBoundingBox!VectorType[] args) {
+	AxisOrientedBoundingBox!VectorType result = args[0];
+	
+	foreach( iterationBoundingBox; args[1..$-1] ) {
+		result = merge(result, iterationBoundingBox);
+	}
+	
+	return result;
 }
