7a042ac3de45ecc2377d57e09eda55043159907d
PtrMan <robertw89@googlemail.com>
2016-08-05 19:31:59 +0200

    [d] : work on the ResourceDag

--- src/d/ResourceDag.d
+++ src/d/ResourceDag.d
@@ -2,79 +2,202 @@ module ResourceDag;
 
 import IDisposable : IDisposable;
 
-// TODO< on dispose check if the counter reaches zero first decrement all internal reference counters, if they reach zero first delete the child then the parent
-
 // is a unified GC for resources
 // does not depend on GC functionality
+
+
+// for the examples ref stands for references,
+// the first number in the brace is the number of dag internal references
+// the second number is the number of external references
+
+// Example 1:
+
+// ref (0) + 1
+// resource a
+//            \
+//             V
+//             ref (2) + 0
+//             resource c
+//             ^
+//            /
+// ref (0) + 2
+// resource b
+//             \
+//              V
+//              ref (1) + 0
+//              resource d
+
+// if resource b gets externally disposed two times we end up with
+
+// ref (0) + 1
+// resource a
+//            \
+//             V
+//             ref (2) + 0
+//             resource c
+//             ^
+//            /
+// ref (0) + 0
+// resource b
+//             \
+//              V
+//              ref (1) + 0
+//              resource d
+
+// so its after to dispose it, after this happened the graph looks like this
+
+// ref (0) + 1
+// resource a
+//            \
+//             V
+//             ref (1) + 0
+//             resource c
+
+//             ref (0) + 0
+//             resource d
+
+// which means resource d can get safly disposed
+
+
 class ResourceDag {
 	public interface IResource : IDisposable {
 
 	}
 
-	public class ResourceNode : IDisposable {
-		protected final this(IResource Resource) {
-			this.Resource = Resource;
+	public /*not static*/ class ResourceNode : IDisposable {
+		protected final this(IResource resource) {
+			this.protectedResource = resource;
 		}
 
 		public void dispose() {
 			disposeFromExternal();
 		}
+		
+		public final void incrementExternalReferenceCounter() {
+			assert(dagReferenceCounterExternal >= 0);
+			dagReferenceCounterExternal++;
+		}
+		
+		public final void decrementExternalReferenceCounter() {
+			assert(dagReferenceCounterExternal >= 1);
+			dagReferenceCounterExternal--;
+		}
 
 		// dispose without time delay
-		public final void disposeImmediatly() {
-
+		public final void disposeFromExternalImmediatly() {
+			if( dagReferenceCounterCombined == 0 ) {
+				assert(!wasDisposed);
+				
+				protectedResource.dispose();
+				wasDisposed = true;
+			}
 		}
 
 		private final void disposeFromExternal() {
-			WasDisposedFromExternal = true;
+			wasDisposedFromExternal = true;
 		}
 
-		public final void addChild(ResourceNode Child) {
+		public final void addChild(ResourceNode child) {
 			// TODO< plausibility checks >
 
-			ChildResourceIndices ~= Child.ResourceDagIndex;
+			childResourceIndices ~= child.resourceDagIndex;
+			resourceNodes[child.resourceDagIndex].dagReferenceCounterInternal++;
 		}
 
-		public final IResource getResource() {
-			return Resource;
+		public final @property IResource resource() {
+			return protectedResource;
+		}
+		
+		public final @property bool isNotReferenced() {
+			return dagReferenceCounterCombined == 0;
 		}
 
-		protected bool WasDisposedFromExternal = false;
+		protected bool wasDisposedFromExternal = false;
+		protected bool wasDisposed = false;
 
-		protected IResource Resource;
+		protected IResource protectedResource;
 
-		protected uint ResourceDagIndex;
-		protected uint[] ChildResourceIndices;
+		protected size_t resourceDagIndex;
+		protected size_t[] childResourceIndices;
 
-		protected int DagReferenceCounter;
+		protected int dagReferenceCounterInternal, dagReferenceCounterExternal;
+		
+		protected final @property int dagReferenceCounterCombined() {
+			assert(dagReferenceCounterInternal >= 0);
+			assert(dagReferenceCounterExternal >= 0);
+			return dagReferenceCounterInternal + dagReferenceCounterExternal;
+		}
 	}
 
 	// doesn't do anything, just holds references to childs
-	public class EntryResourceNode : ResourceNode {
+	public static class EntryResourceNode : ResourceNode {
 		protected final this() {
 			super(null);
 		}
 	}
-
-	public final void disposeFromExternalAll() {
-		// disposes all resources
-
-		// TODO
+	
+	// TODO< maybe its better to first dispose the parent and then the childs and so on >
+	// disposes all resources
+	// disposedAny : indicates if any resources got disposed
+	public final void disposeFromExternalAll(out bool disposedAny) {
+		disposedAny = false;
+		
+		for( size_t resourceNodeI = 0; resourceNodeI < resourceNodes.length; resourceNodeI++ ) {
+			ResourceNode currentResourceNode = resourceNodes[resourceNodeI];
+			
+			if( currentResourceNode is null ) {
+				continue;
+			}
+			
+			if( currentResourceNode.isNotReferenced ) {
+				currentResourceNode.disposeFromExternalImmediatly();
+				disposedAny = true;
+			}
+			
+			if( currentResourceNode.wasDisposed ) {
+				// can only got disposed if both references were zero
+				assert(currentResourceNode.dagReferenceCounterExternal == 0 && currentResourceNode.dagReferenceCounterInternal == 0);
+				
+				decrementInternalReferenceCountersOfChildrenAndSetToNull(resourceNodeI);
+			}
+		}
+	}
+	
+	protected final void decrementInternalReferenceCountersOfChildrenAndSetToNull(size_t resourceNodeIndex) {
+		ResourceNode currentResourceNode = resourceNodes[resourceNodeIndex];
+		
+		// could only be called if both reference counters are zero
+		assert(currentResourceNode.dagReferenceCounterExternal == 0 && currentResourceNode.dagReferenceCounterInternal == 0);
+		
+		// decrement all internal references of the childs
+		foreach( iterationChildIndex; currentResourceNode.childResourceIndices ) {
+			ResourceNode childResourceNode = resourceNodes[iterationChildIndex];
+			
+			if( childResourceNode is null ) {
+				continue;
+			}
+			
+			childResourceNode.dagReferenceCounterInternal--;
+			assert(childResourceNode.dagReferenceCounterInternal >= 0);
+		}
+		
+		// set to null
+		resourceNodes[resourceNodeIndex] = null;
 	}
 
-	public final ResourceNode createNode(IResource Resource) {
-		ResourceNode CreatedResourceNode = new ResourceNode(Resource);
-		ResourceNodes ~= CreatedResourceNode;
-		CreatedResourceNode.ResourceDagIndex = cast(uint)ResourceNodes.length-1;
-		return CreatedResourceNode;
+	public final ResourceNode createNode(IResource resource) {
+		ResourceNode createdResourceNode = new ResourceNode(resource);
+		resourceNodes ~= createdResourceNode;
+		createdResourceNode.resourceDagIndex = cast(size_t)resourceNodes.length-1;
+		return createdResourceNode;
 	}
 
 	public final EntryResourceNode createEntryNode() {
-		EntryResourceNode CreatedEntryResourceNode = new EntryResourceNode();
-		ResourceNodes ~= CreatedEntryResourceNode;
-		CreatedEntryResourceNode.ResourceDagIndex = cast(uint)ResourceNodes.length-1;
-		return CreatedEntryResourceNode;
+		EntryResourceNode createdEntryResourceNode = new EntryResourceNode();
+		resourceNodes ~= createdEntryResourceNode;
+		createdEntryResourceNode.resourceDagIndex = cast(size_t)resourceNodes.length-1;
+		return createdEntryResourceNode;
 	}
 
-	private ResourceNode[] ResourceNodes;
+	private ResourceNode[] resourceNodes;
 }
