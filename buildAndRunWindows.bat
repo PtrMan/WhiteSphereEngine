3e756cb4400ad2fb5dca736df0fcb7e06b474ab0
PtrMan <robertw89@googlemail.com>
2016-08-07 17:39:26 +0200

    [build] modified build to emit and start the exe

--- buildAndRunWindows.bat
+++ buildAndRunWindows.bat
@@ -1,3 +1,4 @@
 set VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_api_dump
 
-rdmd gdi32.lib -g -wi src/d/SwapChainTest1.d
+rdmd --build-only -ofSwapchainTest1.exe gdi32.lib -g -wi src/d/SwapChainTest1.d
+SwapChainTest1
