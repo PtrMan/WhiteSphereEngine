b796be8329003842fc686b4832c1d9fbb6402863
PtrMan <robertw89@googlemail.com>
2016-08-09 22:24:15 +0200

    [d/memory] : added method to FreeList for allocating a element with a predicate

--- src/d/memory/FreeList.d
+++ src/d/memory/FreeList.d
@@ -1,5 +1,7 @@
 module memory.FreeList;
 
+import std.algorithm.mutation : remove;
+
 // simple implementation of freelist with a array
 // a freelist is described here https://en.wikipedia.org/wiki/Free_list
 struct FreeList(Type) {
@@ -19,6 +21,34 @@ struct FreeList(Type) {
 	
 	public final void free(Type value) {
 		addToList(value);
+	}
+	
+	public enum EnumAllocateWhereResult {
+		FOUND,
+		COULDNTFIND,
+		ISEMPTY
+	}
+	
+	public final Type allocateWhere(CheckValueType)(bool function(Type value, CheckValueType) condition, CheckValueType value, out EnumAllocateWhereResult result) {
+		result = EnumAllocateWhereResult.COULDNTFIND;
+		
+		bool isEmpty = list.length == 0;
+		if( isEmpty ) {
+			result = EnumAllocateWhereResult.ISEMPTY;
+			return Type.init;
+		}
+		
+		foreach( i, iterationValue; list ) {
+			if( condition(iterationValue, value) ) {
+				list.remove(i);
+				
+				result = EnumAllocateWhereResult.FOUND;
+				return iterationValue;
+			}
+		}
+		
+		result = EnumAllocateWhereResult.COULDNTFIND;
+		return Type.init;
 	}
 	
 	// used to fill the Freelist
