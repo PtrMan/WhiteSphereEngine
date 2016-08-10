3bc24b108cd037aea1541cfc9e6e6970a1a3dc72
PtrMan <robertw89@googlemail.com>
2016-08-10 21:28:16 +0200

    [build] : do unittests at every start!

--- buildAndRunWindows.bat
+++ buildAndRunWindows.bat
@@ -1,4 +1,4 @@
 set VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_api_dump
 
-rdmd --build-only -ofSwapchainTest1.exe gdi32.lib -g -wi -profile=gc -profile src/d/SwapChainTest1.d
+rdmd --build-only -ofSwapchainTest1.exe gdi32.lib -g -wi -profile=gc -profile -unittest src/d/SwapChainTest1.d
 SwapChainTest1
