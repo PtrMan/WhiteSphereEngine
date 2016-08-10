f40118e59d69c083c77cf50f5d288dd86cc0b8e6
PtrMan <robertw89@googlemail.com>
2016-08-10 21:37:58 +0200

    [resources] : reflect the change in CompileResources.bat

--- CompileResources.bat
+++ CompileResources.bat
@@ -1,3 +1,3 @@
-C:\VulkanSDK\1.0.21.1\Bin\glslangValidator -V resources/engine/graphics/shader/src/TestHardcoded.vert -o TestHardcoded.vert.spv
-C:\VulkanSDK\1.0.21.1\Bin\glslangValidator -V resources/engine/graphics/shader/src/Simple1.frag -o Simple1.frag.spv
+C:\VulkanSDK\1.0.21.1\Bin\glslangValidator -V resourcesSrc/engine/graphics/shader/src/TestHardcoded.vert -o TestHardcoded.vert.spv
+C:\VulkanSDK\1.0.21.1\Bin\glslangValidator -V resourcesSrc/engine/graphics/shader/src/Simple1.frag -o Simple1.frag.spv
 
