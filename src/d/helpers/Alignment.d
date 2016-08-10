f5c4e8ff2355740ca8bc5a7038f3947f32ad44da
PtrMan <robertw89@googlemail.com>
2016-08-10 19:39:33 +0200

    [d] : hlpers/Alignment.d alignAt is now a template

--- src/d/helpers/Alignment.d
+++ src/d/helpers/Alignment.d
@@ -1,14 +1,14 @@
 module helpers.Alignment;
 
 // works for nonpower of two too
-size_t alignAt(size_t value, size_t alignment) {
+Type alignAt(Type)(Type value, Type alignment) {
 	return ((value/alignment) + (((value % alignment) != 0) ? 1 : 0)) * alignment;
 }
 
 unittest {
-	assert(alignAt(0, 4) == 0);
-	assert(alignAt(1, 4) == 4);
-	assert(alignAt(3, 4) == 4);
-	assert(alignAt(4, 4) == 4);
-	assert(alignAt(5, 4) == 8);	
+	assert(alignAt!size_t(0, 4) == 0);
+	assert(alignAt!size_t(1, 4) == 4);
+	assert(alignAt!size_t(3, 4) == 4);
+	assert(alignAt!size_t(4, 4) == 4);
+	assert(alignAt!size_t(5, 4) == 8);	
 }
