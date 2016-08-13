e83efcbbe3f4c5ba1d5e4068f77e145ed7441543
PtrMan <robertw89@googlemail.com>
2016-08-13 17:55:55 +0200

    [resources] : added new shader which does push constant transformation

--- CompileResources.bat
+++ CompileResources.bat
@@ -1,3 +1,4 @@
 C:\VulkanSDK\1.0.21.1\Bin\glslangValidator -V resourcesSrc/engine/graphics/shader/src/TestHardcoded.vert -o TestHardcoded.vert.spv
 C:\VulkanSDK\1.0.21.1\Bin\glslangValidator -V resourcesSrc/engine/graphics/shader/src/Simple1.frag -o Simple1.frag.spv
 C:\VulkanSDK\1.0.21.1\Bin\glslangValidator -V resourcesSrc/engine/graphics/shader/src/SimpleNontransforming_2.vert -o SimpleNontransforming_2.vert.spv
+C:\VulkanSDK\1.0.21.1\Bin\glslangValidator -V resourcesSrc/engine/graphics/shader/src/SimpleTransforming_3.vert -o SimpleTransforming_3.vert.spv
---
+++ resourcesSrc/engine/graphics/shader/src/SimpleTransforming_3.vert
@@ -0,0 +1,14 @@
+#version 400
+#extension GL_ARB_separate_shader_objects : enable
+#extension GL_ARB_shading_language_420pack : enable
+
+layout (location = 0) in vec4 position;
+
+layout(std430, push_constant) uniform PushConstants
+{
+    mat4 mvp;
+} constants;
+
+void main() {
+    gl_Position = constants.mvp * position;
+}
