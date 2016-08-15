5e31fdc755be50d9b76a504fe54cb11f5fb3cfb5
PtrMan <robertw89@googlemail.com>
2016-08-15 18:01:05 +0200

    [build] included the (currently hardcoded path) to erupted, yes we now use erupted, manual start of the program is required

--- buildAndRunWindows.bat
+++ buildAndRunWindows.bat
@@ -1,4 +1,4 @@
 set VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_api_dump
 
-rdmd --build-only -ofSwapchainTest1.exe gdi32.lib -g -wi -profile=gc -profile -unittest src/d/SwapChainTest1.d
+rdmd --build-only -ofSwapchainTest1.exe gdi32.lib -g -wi -profile=gc -unittest -IC:\Users\r0b3\github\ErupteD\source src/d/SwapChainTest1.d
 rem SwapChainTest1
