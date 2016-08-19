2a87d890017bbc659c249b188fccf3d695d91de3
PtrMan <robertw89@googlemail.com>
2016-08-19 18:27:01 +0200

    [D/math] : added better cleaner exponent function, there is still room for improvement

--- src/d/math/Math.d
+++ src/d/math/Math.d
@@ -1,7 +1,16 @@
 module math.Math;
 
+// TODO< remove because of Stackoverflow crap >
 // http://stackoverflow.com/questions/16443682/c-power-of-integer-template-meta-programming
 T exponentInteger(T)(T base, uint exponent) {
 	// (parentheses not required in next line)
     return (exponent == 0) ? 1 : (base * exponentInteger(base, exponent-1));
+}
+
+Type powerByInteger(uint power, Type)(Type number) {
+  Type result = cast(Type)1;
+  foreach( i; 0..power ) {
+    result *= number;
+  }
+  return result;
 }
