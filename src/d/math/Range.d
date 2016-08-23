008b98cca1d07f5c24f2703b70c74033e03012d9
PtrMan <robertw89@googlemail.com>
2016-08-23 20:29:44 +0200

    [D/math] : Range.d : refactoring with enum at compiletime

--- src/d/math/Range.d
+++ src/d/math/Range.d
@@ -1,13 +1,25 @@
 module math.Range;
 
-bool isInRange(float x0, float x1, float x) {
+private bool isInRange(Type)(Type x0, Type x1, Type x) {
 	assert(x0 <= x1);
 
 	return x0 < x && x < x1;
 }
 
-bool isInRangeInclusive(float x0, float x1, float x) {
+private bool isInRangeInclusive(Type)(Type x0, Type x1, Type x) {
 	assert(x0 <= x1);
 
 	return x0 <= x && x <= x1;
+}
+
+enum EnumRangeType {
+	INCLUSIVE,
+	EXCLUSIVE
+}
+
+bool isInRangeEnum(EnumRangeType rangeType, Type)(Type x0, Type x1, Type x) {
+	final switch(rangeType) with(EnumRangeType) {
+		case INCLUSIVE: return isInRangeInclusive(x0, x1, x);
+		case EXCLUSIVE: return isInRange(x0, x1, x);
+	}
 }
