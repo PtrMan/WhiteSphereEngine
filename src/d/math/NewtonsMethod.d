b4ddf9a5574b3f78ccc501aa7ac3138ecb6ac514
PtrMan <robertw89@googlemail.com>
2016-08-23 16:52:19 +0200

    [D/math] : NewtonsMethod.d : forgot to import

--- src/d/math/NewtonsMethod.d
+++ src/d/math/NewtonsMethod.d
@@ -1,5 +1,7 @@
 module math.NewtonsMethod;
 
+import std.math : abs;
+
 // https://en.wikipedia.org/wiki/Newton%27s_method
 Type newtonsMethod(Type)(Type delegate(Type value) f, Type delegate(Type value) fDerivative, Type accuracy, size_t maxRepetitions = -1, Type x = cast(Type)0) {
 	assert(accuracy > cast(Type)0);
