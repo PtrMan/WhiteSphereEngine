acdc7a805c59c136d7969c82189f706bf2843fd4
PtrMan <robertw89@googlemail.com>
2016-08-26 16:49:42 +0200

    [D/serialisation] : added csv helper for reading csv and tsv tables

---
+++ code/d/whiteSphereEngine/serialisation/csv/CsvHelper.d
@@ -0,0 +1,28 @@
+import std.csv;
+import std.array : array;
+import std.conv : to;
+import std.algorithm.searching : find, boyerMooreFinder;
+
+Type[] convertTextToCsv(Type)(string text, char seperator = ',') {
+	return csvReader!Type(text, seperator).array;
+}
+
+Type[] jumpOverHeaderLinesAndReadCsv(Type)(string text, uint numberOfLinesToJumpOver, string seperator = ",")
+in {
+	assert(seperator.length == 1);
+}
+body {
+	foreach( iterationLine; 0..numberOfLinesToJumpOver ) {
+		text = text.find(boyerMooreFinder("\n"))[1..$-1];
+	}
+
+	return convertTextToCsv!Type(text, seperator[0]);
+}
+
+unittest {
+	import std.typecons : Tuple;
+	alias Tuple!(int, float) TestType;
+
+	TestType[] result = jumpOverHeaderLinesAndReadCsv!TestType("aabb\nccdd\n5\t3.0\n2\t1.0\n", 2, "\t");
+	assert(result.length == 2);
+}
