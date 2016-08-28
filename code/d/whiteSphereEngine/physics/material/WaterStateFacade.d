689fd9660d665a09738a525533e51e054c244024
PtrMan <robertw89@googlemail.com>
2016-08-28 23:08:14 +0200

    [D/physics/material] : added WaterStateFacade

---
+++ code/d/whiteSphereEngine/physics/material/WaterStateFacade.d
@@ -0,0 +1,48 @@
+module whiteSphereEngine.physics.material.WaterStateFacade;
+
+import whiteSphereEngine.physics.material.thermodynamics.WaterStateChangeForFluidSteam;
+import whiteSphereEngine.physics.material.SpecificHeatLookup;
+
+// is a abstraction over the different table lookups and thermodynamic calculations of heating the different states of water and the transitions between the states
+
+class WaterStateFacade {
+	final this() {
+		heatLookupWaterSteam = new SpecificHeatLookup(2, 2, 0, 1);
+		heatLookupWaterIce = new SpecificHeatLookup(2, 2, 0, 3);
+		waterStateChangeForFluidSteam = new WaterStateChangeForFluidSteam;
+	}
+
+	final void load() {
+		heatLookupWaterSteam.load("resources/engine/physics/material/Specific heat water vapor.tsv");
+		heatLookupWaterIce.load("resources/engine/physics/material/Properties water ice.tsv");
+		waterStateChangeForFluidSteam.load();
+	}
+
+	final float calcDeltaTemperatureForIce(float temperatureInKelvin, float massInKg, float deltaEnergyInJoules) {
+		assert(temperatureInKelvin <= 273.0f + 0.001f /* epsilon */); // TODO< get constant >
+		return heatLookupWaterIce.calcDeltaTemperatureForIce(temperatureInKelvin, massInKg, deltaEnergyInJoules);
+	}
+
+	final float calcDeltaTemperatureForSteam(float temperatureInKelvin, float massInKg, float deltaEnergyInJoules) {
+		return heatLookupWaterSteam.calcDeltaTemperatureForIce(temperatureInKelvin, massInKg, deltaEnergyInJoules);
+	}
+
+	// all delegations to WaterStateChangeForFluidSteam
+
+	final void calcDeltaTemperatureOfFluid(float absolutePressureInKpa, float startTemperatureInKelvin, float energyDeltaInJoule, float freezingTemperatureInKelvin, out float deltaTemperature, out WaterStateChangeForFluidSteam.EnumStateChange stateChange) {
+		waterStateChangeForFluidSteam.calcDeltaTemperatureOfFluid(absolutePressureInKpa, startTemperatureInKelvin, energyDeltaInJoule, freezingTemperatureInKelvin, deltaTemperature, stateChange);
+	}
+
+	final void calcEvaporationOfFluid(
+		WaterStateChangeForFluidSteam.CalcEvaporationOfFluidParameters parameters,
+		out double deltaFluidMassInKg,
+		out double deltaSteamMassInKg,
+		out float deltaEnergyInJoules,
+		out WaterStateChangeForFluidSteamEnumStateChange evaporationCondensationState
+	) {
+		waterStateChangeForFluidSteam.calcEvaporationOfFluid(parameters, deltaFluidMassInKg, deltaSteamMassInKg, deltaEnergyInJoules, evaporationCondensationState);
+	}
+
+	private SpecificHeatLookup heatLookupWaterSteam, heatLookupWaterIce;
+	private WaterStateChangeForFluidSteam waterStateChangeForFluidSteam;
+}
