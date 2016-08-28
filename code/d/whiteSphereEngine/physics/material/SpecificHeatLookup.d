578875945fafa134da0ab76591525e06c8824850
PtrMan <robertw89@googlemail.com>
2016-08-28 22:18:11 +0200

    [D/physics/material] : Generalized SpecificHeatLookup to lookup in any table

--- code/d/whiteSphereEngine/physics/material/SpecificHeatLookup.d
+++ code/d/whiteSphereEngine/physics/material/SpecificHeatLookup.d
@@ -4,16 +4,27 @@ import whiteSphereEngine.misc.LookupInterpolator;
 
 // looks up the specific heat in a table which is ordered by absolute temperatue in kelvin and calculates the heatchange
 class SpecificHeatLookup {
+	final this(size_t numberOfColumns, uint numberOfSkipedRows, size_t indexOfValue, size_t queryIndex) {
+		this.numberOfColumns = numberOfColumns;
+		this.numberOfSkipedRows = numberOfSkipedRows;
+		this.indexOfValue = indexOfValue;
+		this.queryIndex = queryIndex;
+	}
+
 	final void load(string path) {
 		lookupInterpolator = new LookupInterpolator;
-		lookupInterpolator.parseTsvAndReadIntoLookupTableFromFile(path/*"resources/engine/physics/material/Specific heat water vapor.tsv"*/, 2, 2);
+		lookupInterpolator.parseTsvAndReadIntoLookupTableFromFile(path, numberOfColumns, numberOfSkipedRows);
 	}
 
 	final float calcDeltaTemperature(float steamTemperatureInKelvin, float massInKg, float deltaEnergyInJoules) {
-		const float specificHeatInKiloJoulesPerKilogramPerKelvin = lookupInterpolator.lookupAndInterpolate(steamTemperatureInKelvin, 0, 1);
+		const float specificHeatInKiloJoulesPerKilogramPerKelvin = lookupInterpolator.lookupAndInterpolate(steamTemperatureInKelvin, indexOfValue, queryIndex);
 		const float specificHeatInJoulesPerKilogramPerKelvin = specificHeatInKiloJoulesPerKilogramPerKelvin * 1000.0f;
 		return deltaEnergyInJoules / (specificHeatInJoulesPerKilogramPerKelvin*massInKg); // see http://hyperphysics.phy-astr.gsu.edu/hbase/thermo/spht.html
 	}
 
 	private LookupInterpolator lookupInterpolator;
+
+	private size_t numberOfColumns;
+	private uint numberOfSkipedRows;
+	private size_t indexOfValue, queryIndex;
 }
