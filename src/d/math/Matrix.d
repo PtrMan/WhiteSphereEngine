3e27c448f2d5f01135fad5fa854c183cdcb738fa
PtrMan <robertw89@googlemail.com>
2016-09-27 19:45:53 +0200

    [d/math] : Modified Matrix

--- src/d/math/Matrix.d
+++ src/d/math/Matrix.d
@@ -3,50 +3,74 @@ module math.Matrix;
 import math.NumericSpatialVectors;
 
 // TODO< for more than two dimensions! >
-template Matrix(DataType, uint width, uint height) if (width > 1 && height > 1) {
+template Matrix(DataType, uint width, uint height) if (width >= 1 && height >= 1) {
 	class Matrix {
-		public this() {
-			data = new SpatialVector!(width*height, DataType, false)();
+		final this() {
+		}
+
+		final this(DataType[width*height] content ...) {
+			this.data = SpatialVectorStruct!(width*height, DataType, false).make(content);
 		}
 		
-		public this(DataType[width*height] content ...) {
-			this.data = new SpatialVector!(width*height, DataType, false)(content);
+		final DataType opIndexAssign(DataType value, size_t row, size_t column) {
+			return data[row*width + column] = value;
 		}
 		
-		public final DataType opIndexAssign(DataType value, size_t row, size_t column) {
-			return data.data[row*width + column] = value;
-		}
-		
-		public final DataType opIndex(size_t row, size_t column) {
-			return data.data[row*width + column];
+		final DataType opIndex(size_t row, size_t column) const {
+			return data[row*width + column];
 		}
 
 		final DataType opIndexOpAssign(string op)(DataType rhs, size_t row, size_t column) {
 			static if (op == "+") {
-				return data.data[row*width + column] += rhs;
+				return data[row*width + column] += rhs;
 			}
 			else static if (op == "-") {
-				return data.data[row*width + column] -= rhs;
+				return data[row*width + column] -= rhs;
 			}
 			else static if (op == "*") {
-				return data.data[row*width + column] *= rhs;
+				return data[row*width + column] *= rhs;
 			}
 			else static if (op == "/") {
-				return data.data[row*width + column] /= rhs;
+				return data[row*width + column] /= rhs;
 			}
 			else {
 				static assert(0, "Operator "~op~" not implemented");
 			}
 		}
 		
+		/* uncommented because ptr is not anymore supported by the SpatialVector, because it can be stored as simd or nonsimd array
 		final public @property Type* ptr() {
 			return data.ptr;
 		}
+		*/
 
 		
-		private SpatialVector!(width*height, DataType, false) data;
+		private SpatialVectorStruct!(width*height, DataType, false) data;
 		
-		public enum RAWDATALENGTH = width*height;
-		public alias Type = DataType;
+		enum RAWDATALENGTH = width*height;
+		alias Type = DataType;
 	}
+}
+
+Matrix!(Datatype, size, size) diagonal(Datatype, uint size)(Datatype[size] parameters ...) {
+	alias Matrix!(Datatype, size, size) MatrixType;
+
+	MatrixType result = makeNull!(Datatype, size, size);
+	foreach( i, iterationParameter; parameters ) {
+		result[i, i] = iterationParameter;
+	}
+
+	return result;
+}
+
+Matrix!(Datatype, width, height) makeNull(Datatype, uint width, uint height)() {
+	Matrix!(Datatype, width, width) result = new Matrix!(Datatype, width, width)();
+
+	foreach( row; 0..height ) {
+		foreach( column; 0..width ) {
+			result[row, column] = 0;
+		}
+	}
+
+	return result;
 }
