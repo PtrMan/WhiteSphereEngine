76601bb7582bff586266c5672fcf78a2b531b3e5
PtrMan <robertw89@googlemail.com>
2016-10-09 19:28:44 +0200

    [D/math] : VectorAlias

--- src/d/math/VectorAlias.d
+++ src/d/math/VectorAlias.d
@@ -7,3 +7,5 @@ alias SpatialVectorStruct!(3, double) Vector3p; // p stands for precise
 
 alias SpatialVectorStruct!(2, float) Vector2f;
 
+alias SpatialVectorStruct!(2, uint) Vector2ui;
+alias SpatialVectorStruct!(2, int) Vector2i;
